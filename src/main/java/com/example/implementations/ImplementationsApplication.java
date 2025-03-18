39cdf6dfef5e4d2c190c54af5217098ac021b335
5CG44346Y7\omendez <omendez@nymiz.ai>
2025-03-18 19:23:28 +0100

    Proyecto base e implementación de H2 database

---
+++ src/main/java/com/example/implementations/ImplementationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.implementations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/implementations/ImplementationsApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.implementations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementationsApplicationTest {
+    @Test
+    void contextLoads() {
+
+    }
+}
