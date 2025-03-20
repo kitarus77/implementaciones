a9c3650cdb4b03424b3040cba7d12d4046f73bb6
5CG44346Y7\omendez <omendez@nymiz.ai>
2025-03-20 08:41:50 +0100

    Modificación return ejemplo

--- src/main/java/com/example/implementations/services/HelloWorldService.java
+++ src/main/java/com/example/implementations/services/HelloWorldService.java
@@ -3,12 +3,8 @@ package com.example.implementations.services;
 
 import com.example.implementations.api.HelloApiDelegate;
 import com.example.implementations.model.GetHelloWorld200Response;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class HelloWorldService implements HelloApiDelegate {
@@ -17,6 +13,6 @@ public class HelloWorldService implements HelloApiDelegate {
     public ResponseEntity<GetHelloWorld200Response> getHelloWorld() {
         GetHelloWorld200Response response = new GetHelloWorld200Response();
         response.setMessage("¡Hola, Mundo!");
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return ResponseEntity.ok(response);
     }
 }
